6c7667e40b0852c4f2537fe00dbd1190e12ac8b6
heyen <heyen@Qumge-MBP2.local>
2019-02-25 16:19:45 +0800

    角色变更删除验证

--- app/models/role.rb
+++ app/models/role.rb
@@ -19,11 +19,30 @@ class Role < ActiveRecord::Base
   has_and_belongs_to_many :resources, join_table: 'role_resources'
   has_and_belongs_to_many :users, join_table: 'user_roles'
   has_many :user_roles
-  validates_presence_of :name
+  validates_presence_of :name, :desc
+  validates_uniqueness_of :name, :desc
+
   ROLES = {super_admin: '超级管理员', group_admin: '后勤管理员', normal_admin: '后勤', regional_manager: '大区总监',
            project_manager: '项目经理', project_user: '项目专员', agency: '代理商'}
 
+  before_destroy :check_if_can_be_destroy
 
+  validate :check_if_can_be_change
+
+  def check_if_can_be_destroy
+    if ROLES.map{|k, v| k.to_s}.include?(self.desc_was)
+      errors.add :base, "you can't delete this"
+      throw(:abort)
+    end
+  end
+
+
+
+  def check_if_can_be_change
+    if ROLES.map{|k, v| k.to_s}.include?(self.desc_was) && (self.desc_was != self.desc || self.deleted_at.present?)
+      errors.add :desc, "基础角色不可删除或者变更"
+    end
+  end
 
 
 
@@ -53,6 +72,7 @@ class Role < ActiveRecord::Base
     end
 
   end
+
 
 
 
